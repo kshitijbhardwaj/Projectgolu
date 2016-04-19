97b345673098d1f7ab25e919dd55ec8407345d43
Kshitij Bharadwaj -X (ksbharad - TATA CONSULTANCY SERVICES LIMITED at Cisco) <ksbharad@partnet.cisco.com>
2016-04-19 11:46:53 +0530

    some more edits

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ $(document).ready(function(){
 	$.getJSON("phones.json",function(mobdata){
 		
 		$.each(mobdata,function(key,value){
-          $('ul').append("<li id='"+key+ "'>" +value.name+ "</li>");
-      
+          $('ul').append("<li id='mob-name'>" +value.name+ "</li>");
+          $('ul').append("<img id= 'mob-img' src='"+value.imageUrl+"' alt='avatar' />");
+      	  $('ul').append("<p id='mob-desc'>" +value.snippet+ "</p>");
 		});
 	
 	});
